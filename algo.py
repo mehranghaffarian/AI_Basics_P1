f812341967911e44b88e7aa515e1e2ac27918332
Mehran <mehranghaffarian1381@gmail.com>
2023-11-10 02:35:01 +0330

    trying to resolve DFS solution

--- algo.py
+++ algo.py
@@ -23,16 +23,28 @@ def solve(init_state, init_location, method):
     # 4. you can use 'Set', 'Dictionary', 'OrderedDict', and 'heapq' as efficient data structures.
 
     if method == 'Random':
-        return list(np.random.randint(1, 12+1, 10))
-    
+        return list(np.random.randint(1, 12 + 1, 10))
+
     elif method == 'IDS-DFS':
-        ...
-    
+        fringe = [([], init_state)]
+        for limit in range(1, 10):
+            new_nodes = []
+            for node in fringe:
+                if np.array_equal(node[1], solved_state()):
+                    return np.array(node[0])
+                for i in range(1, 12 + 1):
+                    print("\n\n", fringe, "\n\n")
+                    t = (node[0].append(i), next_state(node[1], i))
+                    new_nodes.append(t)
+            fringe = new_nodes
+        return np.array([])
+
+
     elif method == 'A*':
         ...
 
     elif method == 'BiBFS':
         ...
-    
+
     else:
         return []
