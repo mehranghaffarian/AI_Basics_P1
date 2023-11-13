4cfb150978729e598587248260875f959b1c1f7c
Mehran <mehranghaffarian1381@gmail.com>
2023-11-13 13:29:20 +0330

    all algorithms amended

--- algo.py
+++ algo.py
@@ -13,7 +13,6 @@ def find_reverse_action(action):
 
 
 def resolve_actions(forward_actions, backward_actions):
-    print("\n\nwhole actions:\n", forward_actions, backward_actions, "\n")
     for j in range(1, len(backward_actions) + 1):
         forward_actions.append(backward_actions[-1 * j])
 
@@ -39,52 +38,62 @@ def solve(init_state, init_location, method):
     # 3. use 'next_location()' to obtain the next location of the little cubes when taking an action.
     # 4. you can use 'Set', 'Dictionary', 'OrderedD
 
+    explore_count = 0
+    expand_count = 0
+
     if method == 'Random':
         return list(np.random.randint(1, 12 + 1, 10))
 
     elif method == 'IDS-DFS':
-        expand_count = 0
-        explore_count = 0
         fringe = [([], init_state)]
         for limit in range(1, 10):
             new_fringe = []
             for node in fringe:
+                explore_count += 1
                 for i in range(1, 12 + 1):
+                    expand_count += 1
+
+                    new_state = next_state(node[1], i)
                     actions = list.copy(node[0])
                     actions.append(i)
-                    new_node = (actions, next_state(node[1], i))
+                    new_node = (actions, new_state)
+
                     new_fringe.append(new_node)
-                    expand_count += 1
 
                     if np.array_equal(new_node[1], solved_state()):
                         print("\nNumber of explored nodes: ", explore_count, "\nNumber of expanded Nodes: ",
                               expand_count, "\nSearch depth: ", limit, "\n")
                         return np.array(new_node[0])
-
             fringe = new_fringe
-            explore_count += 1
 
         print("Could not find the answer")
 
     elif method == 'A*':
+        expanded_states = {}
         fringe = [(0 + calculate_heuristic(init_location), [], init_state, init_location)]
-        explore_count = 0
-        expand_count = 0
+
         while True:
             target_node = heapq.heappop(fringe)
 
             explore_count += 1
             for i in range(1, 12 + 1):
+                new_state = next_state(target_node[2], i)
+                state_str = np.array_str(new_state)
+
+                expand_count += 1
+
+                if state_str in expanded_states:
+                    continue
+
                 actions = list.copy(target_node[1])
                 actions.append(i)
                 new_location = next_location(target_node[3], i)
                 new_node = (
                     len(actions) + calculate_heuristic(new_location),
                     actions,
-                    next_state(target_node[2], i),
+                    new_state,
                     new_location)
 
-                expand_count += 1
                 heapq.heappush(fringe, new_node)
 
                 if np.array_equal(new_node[2], solved_state()):
@@ -92,34 +101,7 @@ def solve(init_state, init_location, method):
                           expand_count, "\nSearch depth: ", len(new_node[1]), "\n")
                     return np.array(new_node[1])
 
-        # fringe = [([], init_state, init_location, 0 + calculate_heuristic(init_location))]
-        # explore_count = 0
-        # expand_count = 0
-        # while True:
-        #     target_node = fringe[0]
-        #     for node in fringe:
-        #         new_f = node[3]
-        #         if target_node[3] > new_f:
-        #             target_node = node
-        #
-        #     explore_count += 1
-        #     for i in range(1, 12 + 1):
-        #         actions = list.copy(target_node[0])
-        #         actions.append(i)
-        #         new_location = next_location(target_node[2], i)
-        #         new_node = (
-        #             actions, next_state(target_node[1], i), new_location,
-        #             len(actions) + calculate_heuristic(new_location))
-        #
-        #         expand_count += 1
-        #         fringe.append(new_node)
-        #
-        #         if np.array_equal(new_node[1], solved_state()):
-        #             print("\nNumber of explored nodes: ", explore_count, "\nNumber of expanded Nodes: ",
-        #                   expand_count, "\nSearch depth: ", len(new_node[0]), "\n")
-        #             return np.array(new_node[0])
-        #
-        #     fringe.remove(target_node)
+            expanded_states[np.array_str(target_node[2])] = True
 
     elif method == 'BiBFS':
         forward_fringe = {np.array_str(init_state): ([], init_state)}
@@ -128,15 +110,18 @@ def solve(init_state, init_location, method):
         new_forward_fringe = forward_fringe.copy()
         new_backward_fringe = backward_fringe.copy()
 
-        for limit in range(1, 12):
+        while True:
             curr_forward_fringe = new_forward_fringe.copy()
             curr_backward_fringe = new_backward_fringe.copy()
 
             new_forward_fringe.clear()
             new_backward_fringe.clear()
 
+            explore_count += 1
             for state, node in curr_forward_fringe.items():
                 for i in range(1, 12 + 1):
+                    expand_count += 1
+
                     new_state = next_state(node[1], i)
                     new_key = np.array_str(new_state)
 
@@ -147,16 +132,25 @@ def solve(init_state, init_location, method):
                     new_actions.append(i)
 
                     new_forward_fringe[new_key] = (new_actions, new_state)
+                    forward_fringe[new_key] = (new_actions, new_state)
 
                     if np.array_equal(new_state, solved_state()):
                         print("find solution in straight forward")
+                        print("\nNumber of explored nodes: ", explore_count, "\nNumber of expanded Nodes: ",
+                              expand_count, "\nSearch depth: ", len(new_actions), "\n")
                         return np.array(new_actions)
                     if new_key in backward_fringe:
+                        whole_actions = resolve_actions(new_actions, backward_fringe[new_key][0])
                         print("find solution in forward")
-                        return resolve_actions(new_actions, backward_fringe[new_key][0])
+                        print("\nNumber of explored nodes: ", explore_count, "\nNumber of expanded Nodes: ",
+                              expand_count, "\nSearch depth: ", len(whole_actions), "\n")
+                        return whole_actions
 
+            explore_count += 1
             for state, node in curr_backward_fringe.items():
                 for i in range(1, 12 + 1):
+                    expand_count += 1
+
                     pre_state = next_state(node[1], find_reverse_action(i))
                     new_key = np.array_str(pre_state)
 
@@ -167,10 +161,14 @@ def solve(init_state, init_location, method):
                     new_actions.append(i)
 
                     new_backward_fringe[new_key] = (new_actions, pre_state)
+                    backward_fringe[new_key] = (new_actions, pre_state)
 
                     if new_key in forward_fringe:
+                        whole_actions = resolve_actions(forward_fringe[new_key][0], new_actions)
                         print("find solution in backward")
-                        return resolve_actions(forward_fringe[new_key][0], new_actions)
+                        print("\nNumber of explored nodes: ", explore_count, "\nNumber of expanded Nodes: ",
+                              expand_count, "\nSearch depth: ", len(whole_actions), "\n")
+                        return whole_actions
 
     else:
         return []
